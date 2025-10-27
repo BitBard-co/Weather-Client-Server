155348954f38d32ee5eae658113b23a54681a30c
Patrik <yousefpatrik@gmail.com>
2025-10-27 16:25:53 +0100

    Add files via upload

---
+++ Weather/server/server.h
@@ -0,0 +1,22 @@
+
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <stdio.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+
+// Funktioner
+void func_server(int connfd);
+void start_server(void);
+
+#endif // SERVER_H
